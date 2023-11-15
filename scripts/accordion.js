9a433642341817e27932942b2d7bd5ddd312e00e
thekwakudarko <142811397+thekwakudarko@users.noreply.github.com>
2023-11-15 13:39:55 +0000

    final draft of instruments page complete

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,20 +1,12 @@
-// $(".accordion-item").click(function() {
-//   var isItemOpen = $(this).hasClass("open");
-//   $(".accordion-item").removeClass("open");
-//   if (!isItemOpen) {
-//     $(this).addClass("open");
-//   }
-// });
 
-$('.accordion-header').click(function() {
-    var isItemOpen = $(this).hasClass("open");
+$('.accordion-header').click(function () {
+  var isItemOpen = $(this).hasClass("open");
   $(".accordion-item").removeClass("open");
   if (!isItemOpen) {
     $(this).addClass("open");
   }
-    // Optionally, you can close other open items when one is opened
-    $('.accordion-content').not($(this).next()).addClass('hidden');
 
-    // Toggle the 'hidden' class on the content next to the clicked header
-    $(this).next('.accordion-content').toggleClass('hidden');
-  });
+  $('.accordion-content').not($(this).next()).addClass('hidden');
+
+  $(this).next('.accordion-content').toggleClass('hidden');
+});
