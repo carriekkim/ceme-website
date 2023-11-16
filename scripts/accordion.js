e6558b9519d69ac562e03b9be154cb67efb7a5f8
thekwakudarko <142811397+thekwakudarko@users.noreply.github.com>
2023-11-16 00:49:55 +0000

    revised interactivity

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,10 +1,104 @@
 $(document).ready(function () {
-  $('.accordion-header').click(function () {
-    $('.accordion-content').toggleClass('hidden')
-    if ($('.accordion-content').hasClass('hidden')) {
-      $('.icon').removeClass('rotate-icon');
+  $('#header1').click(function () {
+    $('.accordion-content').not('#content1').addClass('hidden');
+    $('.icon').not('#icon1').removeClass('rotate-icon');
+    $('#content1').toggleClass('hidden')
+    if ($('#content1').hasClass('hidden')) {
+      $('#icon1').removeClass('rotate-icon');
     } else {
-      $('.icon').addClass('rotate-icon');
+      $('#icon1').addClass('rotate-icon');
+    }
+  });
+
+});
+
+$(document).ready(function () {
+  $('#header2').click(function () {
+    $('.accordion-content').not('#content2').addClass('hidden');
+    $('.icon').not('#icon2').removeClass('rotate-icon');
+    $('#content2').toggleClass('hidden')
+    if ($('#content2').hasClass('hidden')) {
+      $('#icon2').removeClass('rotate-icon');
+    } else {
+      $('#icon2').addClass('rotate-icon');
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#header3').click(function () {
+    $('.accordion-content').not('#content3').addClass('hidden');
+    $('.icon').not('#icon3').removeClass('rotate-icon');
+    $('#content3').toggleClass('hidden')
+    if ($('#content3').hasClass('hidden')) {
+      $('#icon3').removeClass('rotate-icon');
+    } else {
+      $('#icon3').addClass('rotate-icon');
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#header4').click(function () {
+    $('.accordion-content').not('#content4').addClass('hidden');
+    $('.icon').not('#icon4').removeClass('rotate-icon');
+    $('#content4').toggleClass('hidden')
+    if ($('#content4').hasClass('hidden')) {
+      $('#icon4').removeClass('rotate-icon');
+    } else {
+      $('#icon4').addClass('rotate-icon');
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#header5').click(function () {
+    $('.accordion-content').not('#content5').addClass('hidden');
+    $('.icon').not('#icon5').removeClass('rotate-icon');
+    $('#content5').toggleClass('hidden')
+    if ($('#content5').hasClass('hidden')) {
+      $('#icon5').removeClass('rotate-icon');
+    } else {
+      $('#icon5').addClass('rotate-icon');
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#header6').click(function () {
+    $('.accordion-content').not('#content6').addClass('hidden');
+    $('.icon').not('#icon6').removeClass('rotate-icon');
+    $('#content6').toggleClass('hidden')
+    if ($('#content6').hasClass('hidden')) {
+      $('#icon6').removeClass('rotate-icon');
+    } else {
+      $('#icon6').addClass('rotate-icon');
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#header7').click(function () {
+    $('.accordion-content').not('#content7').addClass('hidden');
+    $('.icon').not('#icon7').removeClass('rotate-icon');
+    $('#content7').toggleClass('hidden')
+    if ($('#content7').hasClass('hidden')) {
+      $('#icon7').removeClass('rotate-icon');
+    } else {
+      $('#icon7').addClass('rotate-icon');
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#header8').click(function () {
+    $('.accordion-content').not('#content8').addClass('hidden');
+    $('.icon').not('#icon8').removeClass('rotate-icon');
+    $('#content8').toggleClass('hidden')
+    if ($('#content8').hasClass('hidden')) {
+      $('#icon8').removeClass('rotate-icon');
+    } else {
+      $('#icon8').addClass('rotate-icon');
     }
   });
 });
